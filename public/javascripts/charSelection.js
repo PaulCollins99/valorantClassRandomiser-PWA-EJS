7f1b738a93c18c7512c2f74612c6d67d597290ae
Sam May <SamMayWork@users.noreply.github.com>
2020-10-10 21:10:38 +0100

    Refactor ;)

--- public/javascripts/charSelection.js
+++ public/javascripts/charSelection.js
@@ -1,55 +1,31 @@
+/**
+ * Handle click events on the submit button, make sure all of the characters
+ * have a name and a selection that are valid, and then save this to local storage
+ * and put on the UI. (Remove Duplicates).
+ */
+const main = () => {
+    // Read the selected characters and names
+    const selects = document.getElementsByClassName("classSelect")
+    const names = document.getElementsByClassName("nameInput")
+
+    // Make sure every selection is valid and has a name, if it is, push it
+    let validCharacters = []
+    Array.from(selects).map((element, index) => {
+        if ((element && element.value !== "") && 
+          (names[index] && names[index].value !== "")) {
+            validCharacters.push({
+                name: names[index].value,
+                character: selects[index].value
+            })
+        }
+    })
+
+    localStorage.setItem("players", JSON.stringify(validCharacters))
+    localStorage.setItem("playerCount", validCharacters.length)
+
+    let currentWindow = window.location.href
+    window.location.href = currentWindow.substring(0, currentWindow.length - 13) + "/main/" + validCharacters.length + "&" + JSON.stringify(validCharacters)
+}
+
 let submit = document.getElementById("submitButton")
 submit.addEventListener("click", main);
-
-function main() {
-    let selects = document.getElementsByClassName("classSelect")
-    let emptyValue = false;
-    let selectsArray = []
-    for (let i = 0; i < selects.length - 1; i++) {
-        if (selects[i].value == "") {
-            emptyValue = true
-        }
-        selectsArray.push(selects[i].value)
-    }
-
-    let names = document.getElementsByClassName("nameInput")
-    let emptyName = false;
-    console.log(names.length)
-    let namesArray = []
-    for (let i = 0; i < names.length; i++) {
-        if (names[i].value == "") {
-            emptyName = true
-        }
-        namesArray.push(names[i].value)
-    }
-
-    if (emptyValue == false && emptyName == false) {
-        console.log("Valid input")
-        if (checkIfDuplicateExists(namesArray) || checkIfDuplicateExists(selectsArray)) {
-            console.log("remove duplicates");
-        } else {
-            console.log("No Duplicates");
-            let playersOutput = []
-            for (let i = 0; i < names.length; i++) {
-                playersOutput.push({
-                    name: names[i].value,
-                    character: selects[i].value
-                })
-            }
-            localStorage.setItem("players", JSON.stringify(playersOutput))
-            localStorage.setItem("playerCount", playersOutput.length)
-
-            let current = window.location.href
-            window.location.href = current.substring(0, current.length - 13) + "/main/" + playersOutput.length + "&" + JSON.stringify(playersOutput)
-
-        }
-    } else if (emptyValue == true) {
-        console.log("Characters missing")
-    } else {
-        console.log("Player name missing")
-    }
-}
-
-function checkIfDuplicateExists(array) {
-    return new Set(array).size !== array.length
-}
